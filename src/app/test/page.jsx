fd19f86058b19eed34e071645869230ebd139df0
siho <dolphin@kakao.com>
2026-04-24 22:55:08 +0900

    feat: add test page

---
+++ src/app/test/page.jsx
@@ -0,0 +1,11 @@
+import { EmailField } from "@/components/common/layout/EmailField";
+
+export default function Test() {
+    return (
+        <>
+            <div className="w-4/6">
+                <EmailField />
+            </div>
+        </>
+    )
+}
